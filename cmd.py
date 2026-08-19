c9f2892065af021dd02adc764a84371c64d2551c
Kairo <tinaastina123456789@gmail.com>
2026-08-19 15:53:57 +0700

    Create cmd.py

---
+++ cmd.py
@@ -0,0 +1,7 @@
+pkg update && pkg upgrade -y
+pkg install git -y
+pkg install python -y
+python -m pip install pycryptodome
+git clone https://github.com/mave3188/butterfly-
+pkg install nodejs -y && npm install -g @whiskeysockets/baileys pino qrcode-terminal
+cd && cd butterfly- && make run
