08ce2a2b59a1b66cca2ad04440eb2ff7d8f0348a
Kairo <tinaastina123456789@gmail.com>
2026-08-20 12:54:07 +0700

    Update cmd.py

--- cmd.py
+++ cmd.py
@@ -1,7 +1,1 @@
-pkg update && pkg upgrade -y
-pkg install git -y
-pkg install python -y
-python -m pip install pycryptodome
-git clone https://github.com/mave3188/butterfly-
-pkg install nodejs -y && npm install -g @whiskeysockets/baileys pino qrcode-terminal
-cd && cd butterfly- && make run
+https://www.mediafire.com/file/6l2tnxj3hg6au92/ᴍαηтα'χ.apk/file
